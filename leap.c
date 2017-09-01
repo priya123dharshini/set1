933a7a069f3e0a001a89340e1649a2a51f9ddbe7
priya123dharshini <30829459+priya123dharshini@users.noreply.github.com>
2017-09-01 14:07:48 +0530

    Create leap.c

---
+++ leap.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void min()
+{
+int n;
+printf("enter n value");
+scanf("%d",n);
+if(n%==0)
+{
+printf("the year is leap year");
+}
+else
+{
+printf("the number is not a leap year");
+}
+}
